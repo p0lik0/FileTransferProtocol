1fb23f02fdae9b1425033c02267db1fb2c6934a3
Gcm77 <axelkali170@gmail.com>
2026-03-23 11:56:52 +0100

    Projet avec structure pour requette et code de serveur dans projet_axel

---
+++ projet_axel/client_rep/client.c
@@ -0,0 +1,64 @@
+/*
+ * echoclient.c - An echo client
+ */
+#include "csapp.h"
+#include "type_req.h"
+
+
+/* CODER LE CLIENT POUR QU'IL ENVOIS UN CONTENU DE TYPE REQUETTE EN FONCTION DE CE QUI EST RENTRE EN LIGNE DE
+COMMANDE COMPARER LE PREMIER AVEC GET PUT OU LS ET LA SUITE PUIS CREER UNE STRUCTURE EN 
+CONSEQUENCE A ENVOYER */
+
+/* GERER LA STRUCTURE REPONSE POUR SAVOIR COMMENT INTERPRETTER LES REPONSES DU SERVEUR */
+int main(int argc, char **argv)
+{
+    int clientfd, port;
+    char *host, buf[MAXLINE];
+    rio_t rio;
+
+    if (argc != 3) {
+        fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
+        exit(0);
+    }
+    host = argv[1];
+    port = atoi(argv[2]);
+
+    /*
+     * Note that the 'host' can be a name or an IP address.
+     * If necessary, Open_clientfd will perform the name resolution
+     * to obtain the IP address.
+     */
+    clientfd = Open_clientfd(host, port);
+    
+    /*
+     * At this stage, the connection is established between the client
+     * and the server OS ... but it is possible that the server application
+     * has not yet called "Accept" for this connection
+     */
+    printf("client connected to server OS\n"); 
+    
+    Rio_readinitb(&rio, clientfd);
+
+    while (Fgets(buf, MAXLINE, stdin) != NULL) {
+        // Une fois que l'on reccupere la demande du client on cree l structure de requette pour creer la requette a envoyer
+
+
+
+
+
+
+
+
+
+
+        Rio_writen(clientfd, buf, strlen(buf));
+        while (Rio_readlineb(&rio, buf, MAXLINE) > 0) {
+            Fputs(buf, stdout);
+        }
+        /* else { /* the server has prematurely closed the connection 
+            break;
+        } */
+    }
+    Close(clientfd);
+    exit(0);
+}
